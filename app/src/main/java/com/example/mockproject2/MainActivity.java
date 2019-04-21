2e2394fe03b84d21d68c7f8f291332498927406d
ejigunbor <ethanji2@illinois.edu>
2019-04-20 22:04:34 -0500

    Random images and text working.

--- app/src/main/java/com/example/mockproject2/MainActivity.java
+++ app/src/main/java/com/example/mockproject2/MainActivity.java
@@ -9,8 +9,6 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     //the arrays for the images and text strings to be displayed, images and text still need to be added.
-    final String caption1 = "when the midterm has 4 programing problems instead of 3";
-    final String caption2 = "when Geoff walks into office hours";
     int[] memeImages = {
             R.drawable.angry,
             R.drawable.birdcage,
@@ -39,12 +37,18 @@ public class MainActivity extends AppCompatActivity {
             R.drawable.thonk,
             R.drawable.tom,
             R.drawable.celebsitting};
-    final String[] memeCaptions = {caption1, caption2};
+    String[] captions = {
+            "When the midterm has 4 programing problems instead of 3",
+            "When Geoff walks into office hours",
+    };
     final String directions = "Tilt device backwards to randomize image or tilt device forwards to randomize caption.";
     final String noGyroscope = "We couldn't connect to your gyroscope, please use the 'RANDOMIZE' button to generate memes";
     Random random = new Random();
+    //The current and last picked image and text.
     int pickedImage = 0;
-    int lastPicked = 0;
+    int lastImage = 0;
+    int pickedText = 0;
+    int lastText = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,19 +57,26 @@ public class MainActivity extends AppCompatActivity {
         //initializing the caption view, image view, button and instruction view.
         final ImageView displayedImage = findViewById(R.id.displayedImage);
         final TextView instructions = findViewById(R.id.instructions);
-        final TextView caption = findViewById(R.id.Caption);
+        final TextView caption = findViewById(R.id.caption);
         final Button randomize = findViewById(R.id.randomize);
         //making the button randomize image and caption
         randomize.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //remove duplicates.
+                //remove duplicate images.
                 do {
                     pickedImage = random.nextInt(memeImages.length);
-                } while (pickedImage == lastPicked);
-                lastPicked = pickedImage;
+                } while (pickedImage == lastImage);
+                lastImage = pickedImage;
                 //display random image.
                 displayedImage.setImageResource(memeImages[pickedImage]);
+                //remove duplicate text.
+                do {
+                    pickedText = random.nextInt(captions.length);
+                } while (pickedText == lastText);
+                lastText = pickedText;
+                //display random text
+                caption.setText(captions[pickedText]);
             }
         });
         //after we put in the library/API for the gyroscope, we put that code here.
